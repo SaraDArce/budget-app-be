6e1af8b5072f49efc610afb87b36eb6796f6f13c
Sara D'Arce <saradarce@gmail.com>
2022-01-27 21:08:59 -0500

    fe and be connected pt 2

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ const express = require("express");
 //CONFIG
 const app = express();
 
+const transactionsController = require("./controllers/transactionsController.js");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
 //ROUTES
 app.get("/", (req, res) => {
-  res.send("Welcome to Budget App");
+  res.send("Welcome to Your Budget App");
+});
+
+app.use("/transactions", transactionsController);
+
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Page not found" });
 });
 
 //EXPORT
---
+++ controllers/transactionsController.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const transactionRoutes = express.Router();
+const transactionsArr = require("../models/transactions.js");
+
+// /transactions
+transactionRoutes.get("/", (req, res) => {
+  res.json(transactionsArr);
+});
+
+// /transactions/1
+
+transactionRoutes.get("/:index", (req, res) => {
+  const { index } = req.params;
+  if (transactionsArr[index]) {
+    res.json(transactionsArr[index]);
+  } else {
+    res.status(404).json({ error: "Not found" });
+  }
+});
+
+// /transactions
+transactionRoutes.post("/", (req, res) => {
+  let { date, trans, amount, type, category, description, tags } = req.body;
+  if (date && trans && amount) {
+    transactionsArr.push(req.body);
+    res.json(transactionsArr[transactionsArr.length - 1]);
+  } else {
+    res.status(422).json({
+      error: "**Please provide all mandatory fields",
+    });
+  }
+});
+
+// /transactions/1
+transactionRoutes.delete("/:index", (req, res) => {
+  const { index } = req.params;
+  if (transactionsArr[index]) {
+    let removed = transactionsArr.splice(index, 1);
+    res.json(removed[0]);
+  } else {
+    res.status(404).json({ error: "Not found" });
+  }
+});
+
+// /transactions/1
+transactionRoutes.put("/:index", (req, res) => {
+  let { index } = req.params;
+  if (!transactionsArr[index]) {
+    res.status(422).json({
+      error: "Not found",
+    });
+  }
+
+  let { date, trans, amount, type, category, description, tags } = req.body;
+  if (date && trans && amount) {
+    transactionsArr[index] = {
+      date,
+      trans,
+      amount,
+      type,
+      category,
+      description,
+      tags,
+    };
+    res.json(transactionsArr[index]);
+  } else {
+    res.status(422).json({
+      error: "**Please provide all mandatory fields",
+    });
+  }
+});
+
+module.exports = transactionRoutes;
